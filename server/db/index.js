ff51aaa3197de42b724e1576a06a4d386fff9682
Evan Cook <evancookie2@gmail.com>
2018-11-05 23:09:07 -0800

    add setup file

--- server/db/index.js
+++ server/db/index.js
@@ -1,29 +1,29 @@
 const { Pool } = require('pg');
 
 const {
-  DB_USER,
-  DB_HOST,
-  DB_DATABASE,
-  DB_PASSWORD,
-  DB_PORT
+  RWT_DB_USER,
+  RWT_DB_HOST,
+  RWT_DB_DATABASE,
+  RWT_DB_PASSWORD,
+  RWT_DB_PORT
 } = process.env;
 
 if (
-  !DB_USER ||
-  !DB_HOST ||
-  !DB_DATABASE ||
-  !DB_PASSWORD ||
-  !DB_PORT
+  !RWT_DB_USER ||
+  !RWT_DB_HOST ||
+  !RWT_DB_DATABASE ||
+  !RWT_DB_PASSWORD ||
+  !RWT_DB_PORT
 ) {
   throw new Error('Missing required DB env vars');
 }
 
 const database = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_DATABASE,
-  password: DB_PASSWORD,
-  port: DB_PORT
+  user: RWT_DB_USER,
+  host: RWT_DB_HOST,
+  database: RWT_DB_DATABASE,
+  password: RWT_DB_PASSWORD,
+  port: RWT_DB_PORT
 });
 
 
