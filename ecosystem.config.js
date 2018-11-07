66fc972f73d628747dc00280a62fa1e79fb70022
Evan Cook <evancookie2@gmail.com>
2018-11-06 23:47:54 -0800

    temp set deploy branch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-208-165-207.compute-1.amazonaws.com',
       key: '~/.ssh/RWT.pem',
-      ref: 'origin/master',
+      ref: 'origin/deployment',
       repo: 'git@github.com:rockwiththis/rockwiththis-website.git',
       path: '/home/ubuntu/rockwiththis-website',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
