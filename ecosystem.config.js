f96c46b8d9333948fb0897dcfc0cfa536f36ea5f
Evan Cook <evancookie2@gmail.com>
2018-11-15 21:33:11 -0800

    add it back, my mistake

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'rwt-website',
+    script: 'npm',
+    args: 'start',
+    env: { "ENV_CONFIG_FILE": "/home/ubuntu/config.js" }
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-208-165-207.compute-1.amazonaws.com',
+      key: '~/.ssh/RWT.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:rockwiththis/rockwiththis-website.git',
+      path: '/home/ubuntu/rockwiththis-website',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
