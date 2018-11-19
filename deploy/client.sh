57e94fc4d8b37608f67ed72b7db13c8515f43952
Evan Cook <evancookie2@gmail.com>
2018-11-19 02:23:56 -0800

    quick deploy script fix

--- deploy/client.sh
+++ deploy/client.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo
+CURRENT_BRANCH=`git branch | grep \* | cut -d ' ' -f2`
 source deploy/setup.sh
 TARGET_NAME=build
 npm run build
 source deploy/push_to_remote.sh
+git checkout $CURRENT_BRANCH
