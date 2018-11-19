d1efa207365538f278a33df4ac4c2e6957d5aff1
Evan Cook <evancookie2@gmail.com>
2018-11-18 23:19:29 -0800

    check git status better

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/bash   
 
 # Load configuration
 source deploy/config.sh
 
-git diff-index --quiet HEAD -- \
-  || echo "Stash or commit your changes before deployment" && return 1;
+git status -s || echo "Stash or commit your changes before deployment" && return 1;
 
 git checkout $DEPLOY_BRANCH
 git pull
