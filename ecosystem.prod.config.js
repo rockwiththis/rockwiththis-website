4acdf239cdaad0b9891dd369031714b57b76169e
Evan Cook <evancookie2@gmail.com>
2018-11-18 21:16:14 -0800

    more fixes

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: 'git@github.com:rockwiththis/rockwiththis-website.git',
       path: '/home/ubuntu/rockwiththis-website',
       'post-setup': 'npm install && npm run build',
-      'post-deploy': 'pm2 startOrRestart ecosystem.prod.js'
+      'post-deploy': 'pm2 startOrRestart ecosystem.prod.config.js'
     }
   }
 }
