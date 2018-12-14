ecd4a6a4b74559b68433c2a0f525f87406b9b92a
Evan Cook <evancookie2@gmail.com>
2018-12-14 00:40:22 -0800

    return filename

--- server/routes/s3/index.js
+++ server/routes/s3/index.js
@@ -7,55 +7,48 @@ const formidable = require('formidable');
 const fs = require('fs');
 const fileType = require('file-type');
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-
 const s3 = new AWS.S3();
 
 const parseFormRequestFile = request => (
   new Promise(resolve => {
     formidable.IncomingForm().parse(request, (err, fields, fileData) => {
-      /*
-      console.log("Parse Form - Error");
-      console.log(err);
-      console.log("Parse Form - Fields");
-      console.log(fields);
-      console.log("Parse Form - Files");
-      console.log(files);
-      */
       resolve(fileData.file)
     })
   })
 );
 
 const uploadFile = (buffer, name, type) => {
-  console.log({
-    ACL: 'publlic-read',
+  const params = {
+    ACL: 'public-read',
     Body: buffer,
     Bucket: 'rockwiththis',
     ContentType: type.mime,
     Key: `${name}.${type.ext}`
-  });
-  return Promise.resolve("fakepath.jpg");
+  };
+  return s3.upload(params).promise();
 };
 
 router.post('/upload', (req, res) => {
   console.log("received request");
+  console.log(AWS.config.credentials);
   return parseFormRequestFile(req)
     .then(file => {
       const buffer = fs.readFileSync(file.path);
       const type = fileType(buffer);
       const timestamp = Date.now().toString();
-      const fileName = `rockwiththis/${timestamp}-lg`;
+      const fileName = `song-uploads/${timestamp}-lg`;
       return uploadFile(buffer, fileName, type);
     })
-    .then(url => {
-      console.log("saved path")
-      console.log(url);
-      return res.status(200).json({ s3ImageUrl: url })
+    .then(data => {
+      console.log("SUCCESS!!!")
+      console.log(data);
+      return res.status(200).json({ s3ImageUrl: data.Location })
     })
+    .catch(e => {
+      console.log("failure");
+      console.log(e);
+      return res.status(500)
+    });
 });
 
 module.exports = router;
