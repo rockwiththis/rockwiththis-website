9a35fba4cc63f1876a12e6a5e4ccaad468b2fe1a
Evan Cook <evancookie2@gmail.com>
2018-12-13 23:40:58 -0800

    reads buffer successfully

--- server/routes/s3/index.js
+++ server/routes/s3/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const AWS = require('aws-sdk');
+
+const formidable = require('formidable');
+const fs = require('fs');
+const fileType = require('file-type');
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -9,13 +14,21 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-const parseFormRequestFiles = request => {
+const parseFormRequestFile = request => (
   new Promise(resolve => {
-    formidable.IncomingForm().parse(req, (err, fields, files) => {
-      resolve(files)
+    formidable.IncomingForm().parse(request, (err, fields, fileData) => {
+      /*
+      console.log("Parse Form - Error");
+      console.log(err);
+      console.log("Parse Form - Fields");
+      console.log(fields);
+      console.log("Parse Form - Files");
+      console.log(files);
+      */
+      resolve(fileData.file)
     })
   })
-}
+);
 
 const uploadFile = (buffer, name, type) => {
   console.log({
@@ -30,28 +43,18 @@ const uploadFile = (buffer, name, type) => {
 
 router.post('/upload', (req, res) => {
   console.log("received request");
-  console.log(req);
-  return parseFormRequestFiles(req)
-    .then(files => {
-      console.log("parsed form input");
-      console.log(files);
-      const path = files.filetoupload.path;
-      console.log(`path: ${path}`);
-      const buffer = fs.readFileSync(path);
-      console.log(`buffer: ${buffer}`);
+  return parseFormRequestFile(req)
+    .then(file => {
+      const buffer = fs.readFileSync(file.path);
       const type = fileType(buffer);
-      console.log(`type: ${type}`);
       const timestamp = Date.now().toString();
-      const fileName = `bucketFolder/${timestamp}-lg`;
-      console.log(`fileName: ${fileName}`);
-      uploadFile(buffer, fileName, type);
-      return response.status(200).send(data);
+      const fileName = `rockwiththis/${timestamp}-lg`;
+      return uploadFile(buffer, fileName, type);
     })
-    .then(data => {
-      console.log()
-      console.log("final data")
-      console.log(data);
-      return res.status(200).send(data)
+    .then(url => {
+      console.log("saved path")
+      console.log(url);
+      return res.status(200).json({ s3ImageUrl: url })
     })
 });
 
