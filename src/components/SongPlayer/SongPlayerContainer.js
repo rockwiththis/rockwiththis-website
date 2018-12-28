09b5bc25461b7a7e5887a63c1e08d980b2677e2f
Evan Cook <evancookie2@gmail.com>
2018-12-28 13:31:53 -0800

    rerender logic exclusively in individual players

--- src/components/SongPlayer/SongPlayerContainer.js
+++ src/components/SongPlayer/SongPlayerContainer.js
@@ -12,12 +12,6 @@ const propTypes = {
 
 class SongPlayerContainer extends React.Component {
 
-  // only re-render container when we load more songs or toggle play
-  shouldComponentUpdate = nextProps => (
-      nextProps.isPlaying !== this.props.isPlaying ||
-      nextProps.songPosts.length !== this.props.songPosts.length
-  );
-
   isSongPlaying = song => (
       this.props.isPlaying &&
       this.props.currentSongId === song.id
