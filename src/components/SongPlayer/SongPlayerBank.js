bed30bc076e900132e3f23f758dfc8e5b97ce80e
Evan Cook <evancookie2@gmail.com>
2019-01-07 23:21:39 -0800

    get soundcloud players loading too

--- src/components/SongPlayer/SongPlayerBank.js
+++ src/components/SongPlayer/SongPlayerBank.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import SoundCloudWidget from 'soundcloud-widget';
 
-const YT_SONG_LIST_PLAYER_CLASSNAME = i => `song-list-yt-player-${i}`;
+const getYoutubeSongListPlayerId = i => `song-list-yt-player-${i}`;
+const getSoundcloudSongListPlayerId = i => `song-list-sc-player-${i}`;
 
-const YT_MATCH_URL = /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})/;
+const youtubeUrlPattern = /(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})/;
+const getSoundCloudUrl = scid => {
+  const url = encodeURIComponent(`https://api.soundcloud.com/tracks/${scid}`)
+  return `https://w.soundcloud.com/player/?url=${url}`;
+}
 
 class SongPlayerBank extends React.Component {
   constructor(props) {
@@ -18,33 +24,53 @@ class SongPlayerBank extends React.Component {
   shouldComponentUpdate = () => false;
 
   componentDidMount = () => {
-    new Promise(resolve => {
-      const tag = document.createElement('script')
-      tag.src = 'https://www.youtube.com/iframe_api'
-      const firstScriptTag = document.getElementsByTagName('script')[0]
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
-      window.onYouTubeIframeAPIReady = () => resolve(window.YT)
-    })
+    this.loadYoutubeIframeApi()
     .then(YT => {
-      const songListPlayers = this.props.initialSongList.reduce((currPlayers, song, i) => {
-        if (!!song.soundcloud_track_id) return currPlayers
-        else return {
-          ...currPlayers,
-          [song.id]: new YT.Player(YT_SONG_LIST_PLAYER_CLASSNAME(i), {
-            videoId: song.youtube_link.match(YT_MATCH_URL)[1],
-            events: { 'onReady': this.onPlayerReady }
-          })
-        }
-      }, {});
-      console.log("PLAYERS LOADED");
-      console.log(this.props.initialSongList.map(s => s.id));
-      console.log(songListPlayers);
-      console.log(this.props.initialActiveSong);
+      const songListPlayers = this.props.initialSongList.reduce((currPlayers, song, i) => ({
+        ...currPlayers,
+        [song.id]: !!song.soundcloud_track_id ?
+          this.createSoundCloudPlayer(song, i) :
+          this.createYoutubePlayer(YT, song, i)
+      }), {});
+
       this.setState({
         songListPlayers: songListPlayers,
         activePlayer: songListPlayers[this.props.initialActiveSong.id]
       })
     })
+  }
+
+  loadYoutubeIframeApi = () => (
+      new Promise(resolve => {
+        const tag = document.createElement('script')
+        tag.src = 'https://www.youtube.com/iframe_api'
+        const firstScriptTag = document.getElementsByTagName('script')[0]
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+        window.onYouTubeIframeAPIReady = () => resolve(window.YT)
+      })
+  );
+
+  createYoutubePlayer = (YT, song, index) => {
+    const player = new YT.Player(getYoutubeSongListPlayerId(index), {
+      videoId: song.youtube_link.match(youtubeUrlPattern)[1],
+      events: { 'onReady': this.onPlayerReady }
+    });
+    return {
+      play: () => player.playVideo(),
+      pause: () => player.pauseVideo()
+    };
+  }
+
+  createSoundCloudPlayer = (song, index) => {
+    const playerId = getSoundcloudSongListPlayerId(index);
+    document.getElementById(playerId)
+      .setAttribute("src", getSoundCloudUrl(song.soundcloud_track_id));
+
+    const player = new SoundCloudWidget(playerId);
+    return {
+      play: () => player.play(),
+      pause: () => player.pause()
+    };
   }
 
   onPlayerReady = ref => {
@@ -54,24 +80,20 @@ class SongPlayerBank extends React.Component {
 
   playSongListSong = songToPlay => {
     if (!!this.state.activePlayer) {
-      this.state.activePlayer.pauseVideo();
+      this.state.activePlayer.pause();
       this.state.activePlayer.seekTo(0);
     }
-    const songToPlayPlayerData = this.state.songListPlayers[songToPlay.id];
-    if (!!songToPlayPlayerData) {
-      console.log(songToPlayPlayerData);
-      songToPlayPlayerData.playVideo();
-      this.setState({ activePlayer: songToPlayPlayerData });
+    const newActivePlayer = this.state.songListPlayers[songToPlay.id];
+    if (!!newActivePlayer) {
+      newActivePlayer.play();
+      this.setState({ activePlayer: newActivePlayer });
     }
     // TODO handle undefined player data
   }
 
-  playActiveSong = () => {
-    console.log(this.state);
-    this.state.activePlayer.playVideo();
-  }
+  playActiveSong = () => this.state.activePlayer.play();
 
-  pauseActiveSong = () => this.state.activePlayer.pauseVideo();
+  pauseActiveSong = () => this.state.activePlayer.pause();
 
   setSongList = newSongList => {
     // TODO
@@ -82,8 +104,8 @@ class SongPlayerBank extends React.Component {
       <div className="song-player-bank">
         {this.props.initialSongList.map((_, i) => (
             <div className="song-player-song">
-              <div id={`song-list-yt-player-${i}`}>
-              </div>
+              <div id={`song-list-yt-player-${i}`}></div>
+              <iframe id={`song-list-sc-player-${i}`}></iframe>
             </div>
         ))}
       </div>
