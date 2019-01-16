589b25fa4fc5d95c21e8bbae9a7f75ea8064eb64
Evan Cook <evancookie2@gmail.com>
2019-01-15 22:36:28 -0800

    minimize deploy down time

--- deploy/push_to_remote.sh
+++ deploy/push_to_remote.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-printf "Archiving remote $REMOTE_PATH/$TARGET_NAME\n"
-
-ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  cp -r $REMOTE_PATH/$TARGET_NAME $REMOTE_PATH/archive/$TARGET_NAME"
-
-printf "Success!\n"
-
-printf "Pushing from ./$TARGET_NAME to remote $REMOTE_PATH/$TARGET_NAME\n"
-
-ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  rm -rf $REMOTE_PATH/$TARGET_NAME"
+DEST_PATH=$REMOTE_PATH/$TARGET_NAME/$(date +%Y%m%d%H%M%S)
+printf "Pushing from ./$TARGET_NAME to remote $DEST_PATH\n"
 
 find $TARGET_NAME -type f -not -path '*/node_modules*' \
   | xargs tar cf - \
   | ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-    tar xf - -C $REMOTE_PATH"
+    tar xf - -C $DEST_PATH"
+
+printf "Success!\n"
+
+printf "Updating current link\n"
+
+ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
+  ln -sfn $DEST_PATH current"
+
+printf "Success!\n"
+
+printf "Removing oldest $TARGET_NAME from $REMOTE_PATH/$TARGET_NAME\n"
+
+ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
+  ls $REMOVE_PATH/$TARGET_NAME/$TARGET_NAME* \
+  | head -n1 \
+  | xargs rm -rf"
 
 printf "Success!\n"
