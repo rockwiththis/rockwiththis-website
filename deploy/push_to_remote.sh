59ccf0a0a147824522830f22113a3267af8ebfb9
Evan Cook <evancookie2@gmail.com>
2019-01-15 22:54:07 -0800

    iron out some final path details

--- deploy/push_to_remote.sh
+++ deploy/push_to_remote.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-DEST_PATH=$REMOTE_PATH/$TARGET_NAME/$(date +%Y%m%d%H%M%S)
-printf "Pushing from ./$TARGET_NAME to remote $DEST_PATH\n"
+DEST_PATH=$REMOTE_PATH/$TARGET_NAME
+DEST_NAME=$TARGET_NAME-$(date +%Y%m%d%H%M%S)
+printf "Pushing from ./$TARGET_NAME to remote $DEST_PATH/$DEST_NAME\n"
 
 find $TARGET_NAME -type f -not -path '*/node_modules*' \
-  | xargs tar cf - \
+  | xargs tar cf $DEST_NAME - \
   | ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-    mkdir $DEST_PATH && tar xf - -C $DEST_PATH"
+    tar xf - -C $DEST_PATH"
 
 printf "Success!\n"
 
 printf "Updating current link\n"
 
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  ln -sfn $DEST_PATH current"
+  ln -sfn $DEST_PATH/$DEST_NAME $DEST_PATH/current"
 
 printf "Success!\n"
 
 printf "Removing oldest $TARGET_NAME from $REMOTE_PATH/$TARGET_NAME\n"
 
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  ls $REMOVE_PATH/$TARGET_NAME/$TARGET_NAME* \
+  ls $REMOTE_PATH/$TARGET_NAME/$TARGET_NAME* \
   | head -n1 \
   | xargs rm -rf"
 
