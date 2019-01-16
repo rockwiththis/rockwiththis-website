1c5fee13542586bdeb97c676a4582eed568809e3
Evan Cook <evancookie2@gmail.com>
2019-01-16 00:22:58 -0800

    try watching

--- deploy/server.sh
+++ deploy/server.sh
@@ -25,7 +25,7 @@ printf "Starting server process\n"
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
   cd $REMOTE_PATH/server \
   && npm install --production \
-  && pm2 startOrRestart ecosystem.config.js"
+  && pm2 startOrRestart ecosystem.config.js --watch"
 printf "Success!\n"
 
 printf "Returning to branch $CURRENT_BRANCH\n"
