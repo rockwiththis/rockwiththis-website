991b2d50d7d58ed331cd24c9d0c18870953bec32
Dane Iliff <dane@MacBook-Pro-2.local>
2019-01-23 11:12:39 -0800

    Updated spotify links

--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -8,9 +8,9 @@ class SocialLinks extends Component {
         return (
           <div className={`socialLinks ${location.pathname !== "/" ? 'notFixed' : ''}`}>
             <ul>
-            <li><a target="_blank" href="https://open.spotify.com/user/jaredp21/playlist/2eWK5PGSTEl8I5ZvMG5VPS"><i className="im im-spotify"></i></a></li>
-            <li className="sc"><a target="_blank" href="https://soundcloud.com/rockwiththis/sets/rock-with-this"><i className="im im-soundcloud"></i></a></li>
-            <li className="insta"><a target="_blank" href="https://www.instagram.com/rockwiththismusic/"><i className="im im-instagram"></i></a></li>
+            <li><a target="_blank" href="http://bit.ly/SPRockWithThis"><i className="im im-spotify"></i></a></li>
+            <li className="sc"><a target="_blank" href="http://bit.ly/SCRockWithThis"><i className="im im-soundcloud"></i></a></li>
+            <li className="insta"><a target="_blank" href="http://bit.ly/IGRockWithThis"><i className="im im-instagram"></i></a></li>
             <li className="fb"><a target="_blank" href="https://www.facebook.com/rockwiththis/"><i className="im im-facebook"></i></a></li>
             <li className="twitter"><a target="_blank" href="https://www.twitter.com/RWTMusic/"><i className="im im-twitter"></i></a></li>
 
