4da815cd23f08fbba8199dc0c00bf3252e241740
Evan Cook <evancookie2@gmail.com>
2019-01-31 02:24:11 -0800

    add moments route to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const songs = require('./songs');
 const subgenres = require('./subgenres');
 const curators = require('./curators');
+const moments = require('./moments');
 const users = require('./users');
 const s3 = require('./s3');
 
 router.use('/songs', songs);
 router.use('/subgenres', subgenres);
 router.use('/curators', curators);
+router.use('/moments', moments);
 router.use('/users', users);
 router.use('/s3', s3);
 
