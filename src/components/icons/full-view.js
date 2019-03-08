0ce2a0ac8c4fdf0162ce5c3d08e18ab29664e235
Dane Iliff <wdaneiliff@gmail.com>
2019-03-08 14:24:08 -0800

    new full icon

--- src/components/icons/full-view.js
+++ src/components/icons/full-view.js
@@ -4,8 +4,8 @@ export default class FullViewIcon extends Component {
 
   render() {
     return (
-      <svg viewBox="0 0 24 22" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24,2 L9,2 L9,0 L24,0 L24,2 Z M24,5 L9,5 L9,7 L24,7 L24,5 Z M24,10 L0,10 L0,12 L24,12 L24,10 Z M24,15 L0,15 L0,17 L24,17 L24,15 Z M24,20 L0,20 L0,22 L24,22 L24,20 Z M7,0 L0,0 L0,7 L7,7 L7,0 Z" id="Shape"></path>
+      <svg width="20px" height="20px" viewBox="0 0 24 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M4,0 L0,0 L0,4 L4,4 L4,0 Z M7,0 L7,4 L24,4 L24,0 L7,0 Z M0,12 L24,12 L24,8 L0,8 L0,12 Z M0,20 L24,20 L24,16 L0,16 L0,20 Z" id="Shape"></path>
       </svg>
     )
   }
