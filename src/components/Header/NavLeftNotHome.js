baaf0c6c868ab9ee83876345cfffa34b2c930c49
Dane Iliff <wdaneiliff@gmail.com>
2019-03-13 00:09:39 -0700

    buncha shit

--- src/components/Header/NavLeftNotHome.js
+++ src/components/Header/NavLeftNotHome.js
@@ -18,7 +18,8 @@ class NavLeftNotHome extends Component {
 
             <div className="nav-left">
               <NavLink className="nav-link" to="/" activeClassName='none'>
-                  <svg className="singleSongBackButton" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 12l-12-8v5h-12v6h12v5z"/></svg>              </NavLink>
+                  <svg className="singleSongBackButton" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13 7v-6l11 11-11 11v-6h-13v-10z"/></svg>            
+              </NavLink>
             </div>
 
           }
