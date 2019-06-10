d69e9d9bcd3d6883031aba6c42dda13b64ea0a19
Evan Cook <evancookie2@gmail.com>
2019-06-09 18:54:10 -0700

    move headers to sub-component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Helmet } from 'react-helmet';
 
+import Headers from './headers/index';
 import HeroPosts from 'components/HeroGrid/HeroPosts';
 import DiscoverSection from 'components/discover';
 import AutoplayErrorModal from 'components/autoplay-error-modal';
@@ -21,18 +21,7 @@ class Homepage extends Component {
 
   render = () => (
       <div className="homeContainer">
-        <Helmet>
-          <title>Rock With This</title>
-            <meta name="description" content="We don't have every song...just the best you haven't heard." />
-            <meta name="keywords" content="Music Discovery Curation" />
-            <meta name="application-name" content="ROCKWITHTHIS" />
-            <meta name="theme-color" content="#e162f8" />
-            <meta property="og:url" content="http://www.rockwiththis.com/" />
-            <meta property="og:title" content="Rock With This" />
-            <meta property="og:description" content="We don't have every song. Just the best you haven't heard." />
-            <meta property="og:image" content="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-1/p200x200/49844120_667342663682594_5822783533663387648_n.jpg?_nc_cat=104&_nc_ht=scontent-sjc3-1.xx&oh=e0a0be17100a25cf00020b625ecb52cc&oe=5CC6B728" />
-            <meta property="og:image:url" content="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-1/p200x200/49844120_667342663682594_5822783533663387648_n.jpg?_nc_cat=104&_nc_ht=scontent-sjc3-1.xx&oh=e0a0be17100a25cf00020b625ecb52cc&oe=5CC6B728" />
-        </Helmet>
+        <Headers />
 
         <HeroPosts
           {...this.props}
