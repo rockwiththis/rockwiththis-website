2e57b8d90cc865b547b7d8e39415a7470e93fdf1
Evan Cook <evancookie2@gmail.com>
2019-07-16 02:48:04 -0700

    a few small fixes

---
+++ src/store/configureStore.js
@@ -0,0 +1,24 @@
+import thunk from 'redux-thunk'
+import appReducer from '../reducers/index'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxLogger from 'redux-logger'
+
+const composeEnhancers = compose
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default function configureStore() {
+  const middleware = [
+    thunk,
+    reduxLogger
+  ].filter(Boolean);
+  const store = createStore(
+    appReducer,
+    composeEnhancers(
+      applyMiddleware(...middleware)
+    )
+  )
+  store.subscribe(() => {
+    // Will be called everytime the state updates
+  })
+  return store
+}
