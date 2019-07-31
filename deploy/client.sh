55995a1b501112763af2e3f4dfcebfe2641f8908
Evan Cook <evancookie2@gmail.com>
2019-07-30 23:40:24 -0700

    deploy nextjs app

--- deploy/client.sh
+++ deploy/client.sh
@@ -7,7 +7,10 @@ source deploy/setup.sh
 DEST_PATH=$REMOTE_PATH/client
 printf "Archiving remote $DEST_PATH\n"
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  cp -r $REMOTE_PATH/client $REMOTE_PATH/archive/client"
+  rm $REMOTE_PATH/archive/client.tar.gz; \
+  cd $REMOTE_PATH \
+  && tar czf client.tar.gz client \
+  && mv ./client.tar.gz ./archive"
 printf "Success!\n"
 
 printf "Pushing client to remote $DEST_PATH\n"
@@ -15,17 +18,26 @@ printf "Pushing client to remote $DEST_PATH\n"
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
   rm -rf $DEST_PATH"
 
-find ./ -path ./node_modules -prune -o -path ./build -prune -o -path ./.next -prune -o -path ./.git -prune -o -path ./deploy -prune -o -path ./server -prune -o -path ./scripts -prune -o -print \
-  | xargs tar cf client \
-  | ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-    tar xf client -C $REMOTE_PATH"
+tar czf client.tar.gz ./static ./ecosystem.client.config.js ./public ./next.config.js ./next-server.js ./src ./package.json ./pages/
+# find ./ -path ./node_modules -prune -o -path ./build -prune -o -path ./.next -prune -o -path ./.git -prune -o -path ./deploy -prune -o -path ./server -prune -o -path ./scripts -prune -o -print \
+  # | xargs tar czf client
+
+scp -i $REMOTE_SSH_KEY_PATH ./client.tar.gz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+rm ./client.tar.gz
+
+ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
+  mkdir $REMOTE_PATH/client \
+  && tar xzf $REMOTE_PATH/client.tar.gz -C $REMOTE_PATH/client \
+  && rm $REMOTE_PATH/client.tar.gz"
 
 printf "Success!\n"
 
 printf "Starting client process\n"
 ssh -i $REMOTE_SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "\
-  npm install --production \
-  && pm2 startOrRestart ecosystem.config.js"
+  cd $REMOTE_PATH/client \
+  && npm install --production \
+  && NODE_PATH='./src' npm run njs-build \
+  && pm2 startOrRestart ecosystem.client.config.js"
 printf "Success!\n"
 
 printf "Returning to branch $CURRENT_BRANCH\n"
