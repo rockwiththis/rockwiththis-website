483fa56fad99c4a89683865e640f63a49e3933de
Evan Cook <evancookie2@gmail.com>
2019-09-10 23:04:47 -0700

    song grid images always square

--- src/components/discover/views/full-view/desktop/song-grid-song.js
+++ src/components/discover/views/full-view/desktop/song-grid-song.js
@@ -19,6 +19,7 @@ export default class SongGridSong extends Component {
         }
         onClick={this.props.setSongAsSpotlight}
       >
+        <div className="dummy">{/* This lets us size the height the same as the width */}</div>
         <div className="image-container">
           <img
             className="song-image"
@@ -44,12 +45,20 @@ export default class SongGridSong extends Component {
             width: 25%;
             cursor: pointer;
           }
+          .dummy {
+            margin-top: 100%;
+          }
           .image-container {
-            position: relative;
+            position: absolute;
+            top: 0;
             width: 100%;
+            height: 100%;
           }
           .song-image {
+            position: absolute;
+            top: 0;
             width: 100%;
+            height: 100%;
           }
           .hover-overlay {
             position: absolute;
