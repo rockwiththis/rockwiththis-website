64ed5be93fe8fce869198244bb7e4a20cc389118
Evan Cook <evancookie2@gmail.com>
2019-09-16 17:45:56 -0700

    entire truncated song post is link

--- src/components/discover/views/full-view/mobile.js
+++ src/components/discover/views/full-view/mobile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 import { propTypes } from './index';
 import LoadingSpinner from '../loading-spinner';
--- src/components/song-post/truncated.js
+++ src/components/song-post/truncated.js
@@ -18,68 +18,69 @@ export default class TruncatedSongPost extends Component {
   }
 
   render = () => (
-      <div className="truncated-song-post">
+      <Link href={`/songs/[id]?id=${this.props.songData.id}`} as={`/songs/${this.props.songData.id}`}>
+        <div className="truncated-song-post">
 
-        <SongPost {...this.props} />
+          <SongPost {...this.props} />
 
-        <Link href={`/songs/[id]?id=${this.props.songData.id}`} as={`/songs/${this.props.songData.id}`}>
           <div className="see-more">
             ...see more
             <IoIosArrowForward />
           </div>
-        </Link>
-        <hr />
+          <hr />
 
-        <style jsx>{`
-          .truncated-song-post {
-            overflow: hidden;
-          }
-          .see-more {
-            margin: 15px 0;
-            font-size: 10pt;
-            text-align: right;
-          }
-          hr {
-            color: #c9c6cb;
-            height: 1px;
-          }
-        `}</style>
-        <style jsx global>{`
-          .truncated-song-post .song-image {
-            width: 30%;
-            padding-right: 20px;
-            padding-bottom: 20px;
-            box-sizing: border-box;
-          }
-          .truncated-song-post .header-title {
-            width: calc(70% - 40px);
-            font-size: 16pt;
-          }
-          .truncated-song-post .spotify-link {
-            display: none;
-          }
-          .truncated-song-post .song-post-content {
-            height: 125px;
-            position: relative;
-            overflow: hidden;
-          }
-          .truncated-song-post .meta-info {
-            font-size: 12pt;
-          }
-          .truncated-song-post .badges {
-            display: none;
-          }
-          .truncated-song-post .song-post-content p {
-            margin: 0;
-            font-size: 10pt;
-            line-height: 20pt;
-          }
-          .truncated-song-post .song-player-control {
-            display: this.props.hideSongControls ? 'none' : 'default';
-            width: 40px;
-            height: 40px;
-          }
-        `}</style>
-      </div>
+          <style jsx>{`
+            .truncated-song-post {
+              overflow: hidden;
+            }
+            .see-more {
+              margin: 15px 0;
+              font-size: 10pt;
+              text-align: right;
+              cursor: pointer;
+            }
+            hr {
+              color: #c9c6cb;
+              height: 1px;
+            }
+          `}</style>
+          <style jsx global>{`
+            .truncated-song-post .song-image {
+              width: 30%;
+              padding-right: 20px;
+              padding-bottom: 20px;
+              box-sizing: border-box;
+            }
+            .truncated-song-post .header-title {
+              width: calc(70% - 40px);
+              font-size: 16pt;
+            }
+            .truncated-song-post .spotify-link {
+              display: none;
+            }
+            .truncated-song-post .song-post-content {
+              height: 125px;
+              position: relative;
+              overflow: hidden;
+            }
+            .truncated-song-post .meta-info {
+              font-size: 12pt;
+            }
+            .truncated-song-post .badges {
+              display: none;
+            }
+            .truncated-song-post .song-post-content p {
+              margin: 0;
+              font-size: 10pt;
+              line-height: 20pt;
+            }
+            .truncated-song-post .song-player-control {
+              display: this.props.hideSongControls ? 'none' : 'default';
+              width: 40px;
+              height: 40px;
+            }
+          `}</style>
+        </div>
+      </Link>
   )
 }
